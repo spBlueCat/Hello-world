8614a0476b2b6b3b62e031b721c170fe7fdd8e97
panlei <1750198339@qq.com>
2021-05-27 00:07:18 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
  * @Author       : panlei
  * @Date         : 2021-05-26 22:55:59
  * @LastEditors  : panlei
- * @LastEditTime : 2021-05-27 00:01:33
+ * @LastEditTime : 2021-05-27 00:05:50
  * @FilePath     : \Hello World\index.js
  */
 function sum(a, b) {
     return a + b;
 }
 
-let a = summ(1 + 2);
+let a = summ(1, 2);
+
+let b = sum(3, 4);
