557bc79bf84562d8fff08fb1d41c0ddc222e2934
DESKTOP-GHPE8FJ\admin <1750198339@qq.com>
2021-05-26 22:57:39 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @Description  :
+ * @Author       : panlei
+ * @Date         : 2021-05-26 22:55:59
+ * @LastEditors  : panlei
+ * @LastEditTime : 2021-05-26 22:56:22
+ * @FilePath     : \Hello World\index.js
+ */
+function sum(a, b) {
+    return a + b;
+}
