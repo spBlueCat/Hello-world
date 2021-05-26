3c9468f66c50fb61ae8d2bce933e189065b684e9
DESKTOP-GHPE8FJ\admin <1750198339@qq.com>
2021-05-27 00:02:35 +0800

    no message

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  * @Author       : panlei
  * @Date         : 2021-05-26 22:55:59
  * @LastEditors  : panlei
- * @LastEditTime : 2021-05-26 22:56:22
+ * @LastEditTime : 2021-05-27 00:01:33
  * @FilePath     : \Hello World\index.js
  */
 function sum(a, b) {
     return a + b;
 }
+
+let a = summ(1 + 2);
